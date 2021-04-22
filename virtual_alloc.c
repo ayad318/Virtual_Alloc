e5ea05d4300d6810f2c7210dda19a6ead37032e9
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-04-22 15:50:43 +0300

    test

--- virtual_alloc.c
+++ virtual_alloc.c
@@ -141,7 +141,7 @@ void init_allocator(void * heapstart, uint8_t initial_size, uint8_t min_size) {
     int max_num_of_nodes = ((pow(2,initial_size - min_size + 1))-1);
     //printf("position:%d\n max num of node: %d\n intial size:%d\n min size: %d\n",position,max_num_of_nodes,initial_size, min_size);
     virtual_sbrk(sizeof(struct node)*max_num_of_nodes + pow(2,initial_size) - position);
-    int new_position = virtual_sbrk(0) - heapstart;
+    //int new_position = virtual_sbrk(0) - heapstart;
     //printf("new position %d",new_position);
     struct node* block = (struct node*) heapstart;
     block->state = FREE;
