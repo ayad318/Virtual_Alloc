a13e7b9bc6be55cf423b6024e61aea93a02ee078
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-04-22 13:43:13 +0300

    test

--- virtual_alloc.c
+++ virtual_alloc.c
@@ -172,13 +172,13 @@ void * virtual_malloc(void * heapstart, uint32_t size) {
         
         struct node* nd = (struct node*) heapstart + i;
         //struct node nd = nds[i];
-        printf("%p\n",nd);
-        printf("index=%d, size=%d, best_fit_size=%d, state=%d\n",nd->index,nd->size, best_fit_size, nd->state);
+        //printf("%p\n",nd);
+        //printf("index=%d, size=%d, best_fit_size=%d, state=%d\n",nd->index,nd->size, best_fit_size, nd->state);
         if(nd->state == FREE){
             if(nd->size == best_fit_size){
-                printf("hello\n");
+                //printf("hello\n");
                 nd->state = ALLOCATED;
-                printf("index=%d, size=%d, best_fit_size=%d, state=%d\n",nd->index,nd->size, best_fit_size, nd->state);
+                //printf("index=%d, size=%d, best_fit_size=%d, state=%d\n",nd->index,nd->size, best_fit_size, nd->state);
                 return nd->mem_block;
             }
         }
