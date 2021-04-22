be4dc019fe5d856cb43421101c845c51f6dca1a3
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-04-22 11:38:11 +0300

    test

--- virtual_alloc.c
+++ virtual_alloc.c
@@ -193,18 +193,18 @@ void virtual_info(void * heapstart) {
     int index = block->index;
     if(block->state == SPLIT){
         //left node
-        void *left = heapstart + (2*index + 1);
+        void *left = heapstart + (sizeof(struct node)*(2*index + 1));
         virtual_info(left);
         //right node
-        virtual_info(heapstart + (2*index + 2));
+        virtual_info(heapstart + (sizeof(struct node)*(2*index + 2)));
     }
     if(block->state == FREE){
         printf("free %d\n",block->size);
-        void *left = heapstart + (2*index + 1);
+        void *left = heapstart + (sizeof(struct node)*(2*index + 1));
         printf("%p %p\n",block , left);
         virtual_info(left);
         //right node
-        virtual_info(heapstart + (2*index + 2));
+        virtual_info(heapstart + (sizeof(struct node)*(2*index + 2)));
     }
 
     if(block->state == ALLOCATED){
