e6174b810745384cba40488ca04ccb6c872cc3d5
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-04-22 12:37:53 +0300

    test

--- tests.c
+++ tests.c
@@ -8,6 +8,8 @@ void * virtual_heap = NULL;
 
 void * virtual_sbrk(int32_t increment) {
     // Your implementation here (for your testing only)
+    void * sbrk = virtual_heap;
+    void *sbrk = sbrk + increment;
     return (void *)(-1);
 }
 
