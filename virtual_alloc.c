24025ce619bd8d3f1da2c71f2ed41f4ed10f7d7b
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-04-25 13:31:11 +0300

    test

--- virtual_alloc.c
+++ virtual_alloc.c
@@ -327,7 +327,7 @@ void * virtual_realloc(void * heapstart, void * ptr, uint32_t size) {
         return ptr;
     }
     if(overlap_p(newptr,ptr,size)){
-        bcopy(newptr,ptr,size);
+        printf("%p %p %d",newptr,ptr,size);
         return ptr;
     }
     memcpy(newptr,ptr,size);
